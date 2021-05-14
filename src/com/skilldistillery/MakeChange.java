4fc0df4bde6b7885ca740eef2f4b85b74c00c0a4
TMShumway <ryujindra@gmail.com>
2021-05-14 16:49:01 -0600

    Added user prompts and scanner

--- src/com/skilldistillery/MakeChange.java
+++ src/com/skilldistillery/MakeChange.java
@@ -1,10 +1,37 @@
 package com.skilldistillery;
 
+import java.util.Scanner;
+
 public class MakeChange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in); //instantiate scanner object to read in user input
+		
+		printHeader(); 
 
+		//The user is prompted asking for the price of the item.
+		System.out.print("What is the price of the item you'd like to purchase?: ");
+		double purchasePrice = scanner.nextDouble();
+		
+		//The user is then prompted asking how much money was tendered by the customer.
+		System.out.print("How much money was tendered? (xx.xx): ");		
+		double amountTendered = scanner.nextDouble();
+		
+		//Display an appropriate message if the customer provided too little money or the exact amount.
+		System.out.println("Warning! Insufficient money provided to pay for the purchase!");		
+		System.out.println("Spot on! Exact amount of money tendered. No change due.");				
+		
+		//If the amount tendered is more than the cost of the item, display the number of 
+		//bills and coins that should be given to the customer.
+		System.out.println("Your change comes out to: ");
+		
 	}
+
+	private static void printHeader() {
+		System.out.println("******************** Welcome to MakeChange ********************");
+		System.out.println("---------------------------------------------------------------");		
+	}
+
 
 }
